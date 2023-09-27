4598802ab3fe5f4213bb34d3086d63091dbbc1ae
RadHead600 <125551394+RadHead600@users.noreply.github.com>
2023-09-27 21:43:52 +0400

    Update UpgradeStatsUI.cs

--- Assets/Scripts/UI/UpgradeStatsUI.cs
+++ Assets/Scripts/UI/UpgradeStatsUI.cs
@@ -7,7 +7,9 @@ public class UpgradeStatsUI : UpgradeUICard
             InfoText.text = CardInformation.LastValue;
             return;
         }
+        private int decimalPlaces = 2;
         
-        InfoText.text = System.Math.Round(System.Convert.ToDouble(CardInformation.LastValue), 2).ToString();
+        InfoText.text = System.Math.Round(System.Convert.ToDouble
+        (CardInformation.LastValue), decimalPlaces ).ToString();
     }
 }
