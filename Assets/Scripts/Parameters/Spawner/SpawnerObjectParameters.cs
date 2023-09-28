90ab102a32442a01e23a9c89eb8b3e3fb40a56fa
RadHead600 <125551394+RadHead600@users.noreply.github.com>
2023-09-28 12:33:40 +0400

    Update SpawnerObjectParameters.cs

--- Assets/Scripts/Parameters/Spawner/SpawnerObjectParameters.cs
+++ Assets/Scripts/Parameters/Spawner/SpawnerObjectParameters.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 [Serializable]
 public class SpawnerObjectParameters
 {
+    private float _minChanceValue = 0f;
+    private float _maxChanceValue = 100f;
+    
     [SerializeField] private Unit _spawnPrefab;
-    [SerializeField][Range(0, 100)] private float _chance;
-
+    [SerializeField][Range(_minChanceValue, _maxChanceValue)] private float _chance;
+    
+    public float MinChanceValueForFields => _minChanceValue;
+    public float MaxChanceValueForFields => _maxChanceValue;
     public Unit SpawnPrefab => _spawnPrefab;
     public float Chance => _chance;
 }
