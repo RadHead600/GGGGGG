2b7923f93f59f0a252f50ac83a4cb7bce7ceaea0
RadHead600 <125551394+RadHead600@users.noreply.github.com>
2023-09-28 12:35:42 +0400

    Update SpawnerParameters.cs

--- Assets/Scripts/Parameters/Spawner/SpawnerParameters.cs
+++ Assets/Scripts/Parameters/Spawner/SpawnerParameters.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SpawnerParameters", menuName = "CustomParameters/SpawnerParameters/StandartSpawnerParameters")]
 public class SpawnerParameters : ScriptableObject
 {
-    [Header("Generation objects.")]
+    private int _minValue = 0;
+    
     [SerializeField] private List<SpawnerObjectParameters> _spawnPrefabs;
-    [Header("The maximum _value of the objects on the wave.")]
-    [SerializeField][Min(0)] private int _maxCountObjects;
-    [Header("The minimum _value of the objects on the wave.")]
-    [SerializeField][Min(0)] private int _minCountObjects;
-    [Header("Reduction of time between waves as a percentage for each level.")]
-    [SerializeField][Min(0)] private float _reduceReloadingTime;
-    [Header("The starting _value of the time between waves of objects.")]
-    [SerializeField][Min(0)] private float _maxReloadingTime;
-    [Header("Minimum _value of the time between waves of objects.")]
-    [SerializeField][Min(0)] private float _minReloadingTime;
+    [SerializeField][Min(_minValue)] private int _maxCountObjects;
+    [SerializeField][Min(_minValue)] private int _minCountObjects;
+    [SerializeField][Min(_minValue)] private float _reduceReloadingTime;
+    [SerializeField][Min(_minValue)] private float _maxReloadingTime;
+    [SerializeField][Min(_minValue)] private float _minReloadingTime;
 
+    public int MinValue => _minValue;
     public List<SpawnerObjectParameters> SpawnPrefabs => _spawnPrefabs;
     public int MaxCountObjects => _maxCountObjects;
     public int MinCountObjects => _minCountObjects;
