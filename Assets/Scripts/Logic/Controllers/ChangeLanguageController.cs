8f3cc3616b001d6160d777b2f287cdd8960cfce2
RadHead600 <125551394+RadHead600@users.noreply.github.com>
2023-09-28 13:32:19 +0400

    Update ChangeLanguageController.cs

--- Assets/Scripts/Logic/Controllers/ChangeLanguageController.cs
+++ Assets/Scripts/Logic/Controllers/ChangeLanguageController.cs
@@ -4,36 +4,45 @@ using UnityEngine;
 
 public class ChangeLanguageController : Singleton<ChangeLanguageController>
 {
+    private const int RussianLanguageIndex = 0;
+    private const int EnglishLanguageIndex = 1;
+    private const int TurkishLanguageIndex = 2;
+    
     [DllImport("__Internal")]
     private static extern string GetLang();
 
     public void SetLanguage()
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
+        #if UNITY_WEBGL && !UNITY_EDITOR
+        
         string lang = GetLang();
+        
         if (lang == "ru")
         {
-            LangsList.SetLanguage(0, true);
+            LangsList.SetLanguage(RussianLanguageIndex, true);
             return;
         }
         if (lang == "en")
         {
-            LangsList.SetLanguage(1, true);
+            LangsList.SetLanguage(EnglishLanguageIndex, true);
             return;
         }
         if (lang == "tr")
         {
-            LangsList.SetLanguage(2, true);
+            LangsList.SetLanguage(TurkishLanguageIndex, true);
             return;
         }
-#elif UNITY_2020_1_OR_NEWER
+        
+        #elif UNITY_2020_1_OR_NEWER
+        
         if (Application.systemLanguage == SystemLanguage.Russian)
-            LangsList.SetLanguage(0, true);
+            LangsList.SetLanguage(RussianLanguageIndex, true);
         if (Application.systemLanguage == SystemLanguage.English)
-            LangsList.SetLanguage(1, true);
+            LangsList.SetLanguage(EnglishLanguageIndex, true);
         if (Application.systemLanguage == SystemLanguage.Turkish)
-            LangsList.SetLanguage(2, true);
-#endif
+            LangsList.SetLanguage(TurkishLanguageIndex, true);
+            
+        #endif
     }
 
     public void ChangeLang (TMP_Dropdown tmp)
