033491af9cecd77d650203d7a5bf17bf33fe444c
RadHead600 <125551394+RadHead600@users.noreply.github.com>
2023-09-28 12:23:06 +0400

    Update UpgradePointsParameters.cs

--- Assets/Scripts/Parameters/Upgrades/UpgradePointsParameters.cs
+++ Assets/Scripts/Parameters/Upgrades/UpgradePointsParameters.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "UpgradePointsParameters", menuName = "CustomParameters/UpgradesParameters/UpgradePointsParameters")]
 public class UpgradePointsParameters : ScriptableObject
 {
-    [Header("How long does it take to complete a level to get a gem?")]
-    [SerializeField][Range(0, 1)] private List<float> _timesToPoints;
-    [Header("how many _gems per time?")]
-    [SerializeField][Min(0)] private int _numOfPoints;
+    private float _minTimeToPointsValue = 0f;
+    private float _maxTimeToPointsValue = 1f;
+    private int _minNumOfPointsValue = 0;
+    
+    [SerializeField][Range(_minTimeToPointsValue,_maxTimeToPointsValue)] private List<float> _timesToPoints;
+    [SerializeField][Min(_minNumOfPointsValue)] private int _numOfPoints;
 
+    public float MinTimeToPointsValue => _minTimeToPointsValue;
+    public float MaxTimeToPointsValue => _maxTimeToPointsValue;
+    public int MinNumOfPointsValue => _minNumOfPointsValue;
     public List<float> TimesToPoints => _timesToPoints;
     public int NumOfPoints => _numOfPoints;
 }
