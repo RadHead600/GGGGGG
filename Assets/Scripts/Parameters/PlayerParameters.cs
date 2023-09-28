6e3d462d0106cffce2d084f1cc1d0d947a22f74a
RadHead600 <125551394+RadHead600@users.noreply.github.com>
2023-09-28 12:14:55 +0400

    Update PlayerParameters.cs

--- Assets/Scripts/Parameters/PlayerParameters.cs
+++ Assets/Scripts/Parameters/PlayerParameters.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerParameters", menuName = "CustomParameters/PlayerParameters")]
 public class PlayerParameters : UnitParameters
 {
-    [Header("waiting time before restarting the death scene")]
     [SerializeField] private float _timeBeforRestartScene;
 
     public float TimeBeforRestartScene => _timeBeforRestartScene;
