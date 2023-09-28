d3cc9190ad2d348c3e02f5a51116c0d23bb94e85
RadHead600 <125551394+RadHead600@users.noreply.github.com>
2023-09-28 11:32:08 +0400

    Update EnemySpawner.cs

--- Assets/Scripts/Spawner/EnemySpawner.cs
+++ Assets/Scripts/Spawner/EnemySpawner.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class EnemySpawner : Spawner
 {
+    private const int InitialCountWaves = 1;
+    private const int IncreaseEnemiesPercentage = 100;
+    
     private int _objectsCount;
     private int _wavesToPassed;
     private float _enemiesReloadingTime;
@@ -13,22 +16,26 @@ public class EnemySpawner : Spawner
 
     public override void StartSpawner()
     {
-        _enemiesReloadingTime = SpawnerParameters.MaxReloadingTime - Mathf.Pow(GameInformation.Instance.Information.PassedLevel, SpawnerParameters.ReduceReloadingTime);
+        _enemiesReloadingTime = SpawnerParameters.MaxReloadingTime - 
+        Mathf.Pow(GameInformation.Instance.Information.PassedLevel, SpawnerParameters.ReduceReloadingTime);
         
         if (_enemiesReloadingTime < SpawnerParameters.MinReloadingTime)
             _enemiesReloadingTime = SpawnerParameters.MinReloadingTime;
         if (_spawnerCoroutine == null)
-            _spawnerCoroutine = StartCoroutine(StartEnemiesSpawner((SpawnerEnemiesParameters)SpawnerParameters, _enemiesReloadingTime, 1));
+            _spawnerCoroutine = StartCoroutine(StartEnemiesSpawner((SpawnerEnemiesParameters)
+            SpawnerParameters, _enemiesReloadingTime, InitialCountWaves));
     }
 
     private IEnumerator StartEnemiesSpawner(SpawnerEnemiesParameters parameters, float reloadingTime, int countWaves)
     {
-        _objectsCount = parameters.MinCountObjects + (int)((float)parameters.MinCountObjects * ((float)parameters.IncreaseEnemies / 100));
+        _objectsCount = parameters.MinCountObjects + (int)((float)parameters.MinCountObjects * 
+        ((float)parameters.IncreaseEnemies / IncreaseEnemiesPercentage));
         
         if (_objectsCount > parameters.MaxCountObjects)
             _objectsCount = parameters.MaxCountObjects;
         CreateObjects(_objectsCount, reloadingTime);
-        _wavesToPassed = parameters.MinWaves + (int)((float)GameInformation.Instance.Information.PassedLevel / (float)parameters.NumLevelForAddWaves);
+        _wavesToPassed = parameters.MinWaves + (int)((float)GameInformation.Instance.Information.PassedLevel / 
+        (float)parameters.NumLevelForAddWaves);
         
         if (countWaves >= _wavesToPassed)
         {
